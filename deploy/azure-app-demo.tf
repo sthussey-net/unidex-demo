e04179ad6c67d75ee66d264fc8283f64609b6079
Scott Hussey <sthussey@gmail.com>
2020-05-22 16:13:02 -0500

    [deploy] Add webapp deployment
    - Create the web app definition in Terraform
    - Add a Github secret to Terraform to allow Github Actions to deploy
    - Define the Github Actions workflow for building and deploying

--- deploy/azure-app-demo.tf
+++ deploy/azure-app-demo.tf
@@ -8,25 +8,61 @@ provider "azurerm" {
   features {}
 }
 
-# Create an application
+provider "github" {
+  token = var.github-token
+  organization = var.app-repo-org
+  individual = false
+  version = "~> 2.8"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+# Generate a random password for the deployer SP
+resource "random_password" "deployer-password" {
+  length = 24
+  special = true
+  override_special = "!#$%^&*()"
+}
+
+# Create the Unidex application
 resource "azuread_application" "unidex" {
   name = "UnidexDemo"
 }
 
-# Create a service principal
-resource "azuread_service_principal" "SP-Unidex-Run" {
-  application_id = azuread_application.unidex.application_id
+# Create an application for the Unidex deployer
+resource "azuread_application" "unidex-deployer" {
+  name = "UnidexDeploy"
 }
 
-resource "azurerm_resource_group" "unidex-demo" {
+# Create a service principal to deploy the application
+resource "azuread_service_principal" "SP-Unidex-Deploy" {
+  application_id = azuread_application.unidex-deployer.application_id
+}
+
+# Set a password that will by synced to a Github Action secret
+resource "azuread_service_principal_password" "SP-Unidex-Deploy-Password" {
+  service_principal_id = azuread_service_principal.SP-Unidex-Deploy.id
+  value = random_password.deployer-password.result
+  # expire after 30 days
+  end_date_relative = "720h"
+}
+
+resource "azurerm_resource_group" "rg-unidex-demo" {
   name = "rg-unidex-demo"
   location = "Central US"
 }
 
-resource "azurerm_app_service_plan" "unidex-demo" {
+resource "azurerm_user_assigned_identity" "SP-Unidex-Run" {
+  name = "SP-Unidex-Run"
+  resource_group_name = azurerm_resource_group.rg-unidex-demo.name
+  location = azurerm_resource_group.rg-unidex-demo.location
+}
+
+resource "azurerm_app_service_plan" "asp-unidex-demo" {
   name = "asp-unidex-demo"
-  location = azurerm_resource_group.unidex-demo.location
-  resource_group_name = azurerm_resource_group.unidex-demo.name
+  location = azurerm_resource_group.rg-unidex-demo.location
+  resource_group_name = azurerm_resource_group.rg-unidex-demo.name
   kind = "Linux"
   reserved = true
 
@@ -34,4 +70,34 @@ resource "azurerm_app_service_plan" "unidex-demo" {
     tier = "Free"
     size = "F1"
   }
+
+}
+
+resource "azurerm_app_service" "as-unidex-demo" {
+  name = "as-unidex-demo"
+  location = azurerm_resource_group.rg-unidex-demo.location
+  resource_group_name = azurerm_resource_group.rg-unidex-demo.name
+  app_service_plan_id = azurerm_app_service_plan.asp-unidex-demo.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    use_32_bit_worker_process = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+    identity_ids = [azurerm_user_assigned_identity.SP-Unidex-Run.id]
+  }
+}
+
+resource "azurerm_role_assignment" "SP-Unidex-Deploy-Contributor" {
+  scope = azurerm_app_service.as-unidex-demo.id
+  role_definition_name = "Contributor"
+  principal_id = azuread_service_principal.SP-Unidex-Deploy.object_id
+}
+
+resource "github_actions_secret" "SP-Unidex-Deploy-Password" {
+  repository = var.app-repo
+  secret_name = "SP_Unidex_Deploy_Password"
+  plaintext_value = random_password.deployer-password.result
 }
---
+++ deploy/variables.tf
@@ -0,0 +1,9 @@
+variable "app-repo-org" {
+  type = string
+  default = "sthussey-net"
+}
+
+variable "app-repo" {
+  type = string
+  default = "unidex-demo"
+}
